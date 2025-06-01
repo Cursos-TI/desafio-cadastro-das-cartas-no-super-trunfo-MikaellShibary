fd4f1ada267f0363e00917ef56c24576a1e7583d
Mikaell Victor <mikaell.victor@hotmail.com>
2025-06-01 10:40:17 +0000

    Correções de BUGs

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,41 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
-
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    //declaração dos utilitarios
+    int turistico, populacao;
+    char estado [40];
+    char nome_cidade [30];
+    char ID_cidade [40];
+    float pib, area;
+// Coleta dos dados dos estados para as cartas
+    printf ("Desafio Super Trunfo - Países\n");
 
+    printf ("Estado: ");
+    fgets (estado, sizeof(estado), stdin);
+
+    printf ("Nome da cidade: ");
+    fgets (nome_cidade, sizeof(nome_cidade), stdin);
+
+    printf ("População total da cidade: %d \n");
+    scanf (" %d, &populacao");
+
+    printf("Qual PIB da cidade: %.f \n");
+    scanf ("%.f, &pib");
+
+    printf ("Quantos pontos turistico a cidade possui: %d \n");
+    scanf ( "%d, &turistico");
+
+    printf ("Qual a área total da cidade: %2f Km²\n");
+    scanf ("%.f, &area");
+// Imprimir na tela o restultado dos dados coletados acima
+    printf ("Super Trunfo - Países \n");
+
+    printf ("Identificação da cidade: %s \n.", ID_cidade);
+    printf ("Estado: %s \n", estado);
+    printf ("Nome da cidade: %s \n.", nome_cidade);
+    printf ("Cidade de %s", ID_cidade, "possui total de %d pessoas. \n", populacao);
+    printf ("Cidade de %s", ID_cidade ,"tem sua área de %.2f metros quadrados.", area);
+    printf ("Cidade de %s", ID_cidade ,"tem %d pontos turístico.\n", turistico);
+    printf ("Cidade de %s", ID_cidade ,"tem seu Podruto Interno Bruto - PIB de: %.3f", pib);
     return 0;
 }
